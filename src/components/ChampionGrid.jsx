e8ef48f508978890028c63b09a8e53a99cf8329a
G1DEON <165989663+G1DEONdt@users.noreply.github.com>
2024-12-13 19:56:16 +1100

    Move to laptop

--- src/components/ChampionGrid.jsx
+++ src/components/ChampionGrid.jsx
@@ -21,46 +21,13 @@ async function getData() {
       }
 }
 
-function ChampionGridIcons(data){
-  
-  useEffect(() => {
-    getData().then((data) => {
-      const array = Object.values(data);
-      // console.log(array);
-      return(
-        <div className='flex'>
-          <h2>WORKING</h2>
-          {/* {array.map((champion) => {
-            let source = "https://ddragon.leagueoflegends.com/cdn/14.23.1/img/champion/" + champion.image.full;
-            <div>
-              <p>{champion.name}</p>
-            </div>
-          })} */}
-        </div>
-      )
-    })
-  }, [])
-}
-
-function GridIcons() {
-  const [data, setData] = useState(null);
-  const [dataArray, setDataArray] = useState(null);
+export default function ChampionGrid() {
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     getData()
-    .then((response) => setData(response))
-    .then(setDataArray(Object.values(data))).then(console.log(dataArray));
-    // .then(() => { setDataArray(Object.values(data))})
-    // .then(console.log(dataArray));
+    .then((response) => setData(Object.values(response)))
   }, []);
-  return(<></>)
-}
-
-
-
-export default function ChampionGrid() {
-
-  
 
   return (
     <div className=' col-start-2 col-end-5 row-start-3 -row-end-3 '>
@@ -69,7 +36,14 @@ export default function ChampionGrid() {
         <input className=" w-1/2 border-2 border-amber-600 bg-transparent px-4 text-amber-600 " placeholder="Search..." type="text" name="champion" id="champion" />
       </div>
       <div className='bg-gray-900 w-full h-full shadow-[inset_0px_0px_25px_0px_rgb(0,0,0)]'>
-        <GridIcons />
+        {data.map((champion, index) => {
+          let source = "https://ddragon.leagueoflegends.com/cdn/14.23.1/img/champion/" + champion.image.full;
+          return (
+            <div key={index}>
+              <img src={source}></img>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
